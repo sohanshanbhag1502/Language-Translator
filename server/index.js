8b0247df17dd2ba1af64815c4ec033bda462e949
Sohan Shanbhag <127968647+sohanshanbhag1502@users.noreply.github.com>
2023-11-24 23:32:55 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -16,6 +16,7 @@ app.post('/login',(req, res)=>{
     res.setHeader("Access-Control-Allow-Credentials", "false");
     res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
     res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
+    console.log(process.env.MONGODB_URL)
     const {username, password}=req.body;
     usersModel.findOne({username:username})
     .then((user)=>{
