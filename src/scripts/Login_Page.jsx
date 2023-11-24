f96b49c316eee0839d2f5f931c683b003e028eb3
Sohan Shanbhag <127968647+sohanshanbhag1502@users.noreply.github.com>
2023-11-24 19:25:13 +0530

    Update Login_Page.jsx

--- src/scripts/Login_Page.jsx
+++ src/scripts/Login_Page.jsx
@@ -120,7 +120,7 @@ function LoginBox(){
     axios.defaults.withCredentials = true;
     const login=(e)=>{
         e.preventDefault()
-        axios.post("http://localhost:3001/login",{username, password})
+        axios.post("https://language-translator-backend.vercel.app/login",{username, password})
         .then((result)=>{
             if (result.data==="Success"){
                 window.loggedin=true
