d04334b2b8111b256f9cab4d471f342c77585a64
Sohan Shanbhag <127968647+sohanshanbhag1502@users.noreply.github.com>
2023-11-24 19:10:48 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -35,6 +35,6 @@ app.post('/userdetails',(req,res)=>{
     .catch(err=>res.json(err))
 })
 
-app.listen(process.env.PORT,()=>{
+app.listen(process.env.PORT || 3001,()=>{
     console.log("server is running")
 })
